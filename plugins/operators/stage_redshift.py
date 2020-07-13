f34c825b20b9b40e38db7e9f35fa928f20d69593
karenlo08 <49169212+karenlo08@users.noreply.github.com>
2020-07-13 10:52:55 -0700

    Update stage_redshift.py

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,44 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
+    copy_query = "COPY {} FROM '{}' ACCESS_KEY_ID '{}' SECRET_ACCESS_KEY '{}'"
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credential_id="",
+                 table_name = "",
+                 s3_bucket="",
+                 s3_key = "",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credential_id = aws_credential_id
+        self.table_name = table_name
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.execution_date = kwargs.get('execution_date')
+        
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credential_id)
+        credentials = aws_hook.get_credentials()
 
-
+        s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+        self.log.info(f"Picking staging file for table {self.table_name} from location : {s3_path}")
+       
+        copy_query = self.copy_query.format(self.table_name, s3_path, credentials.access_key, credentials.secret_key)
+       
+        redshift_hook = PostgresHook(postgres_conn_id = self.redshift_conn_id)
+        
+        redshift_hook.run(copy_query)
+        self.log.info(f"Table {self.table_name} staged successfully!!")
+        
 
 
 
