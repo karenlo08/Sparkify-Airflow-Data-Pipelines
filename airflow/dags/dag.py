76d712825e42ad867f077004b88e1bffcb8de260
karenlo08 <49169212+karenlo08@users.noreply.github.com>
2020-07-14 10:50:57 -0700

    Update dag.py

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -62,28 +62,32 @@ load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
     dag=dag,
     redshift_conn_id = 'redshift',
-    sql_query = SqlQueries.user_table_insert
+    sql_query = SqlQueries.user_table_insert,
+    table_name = "users"
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
     dag=dag,
     redshift_conn_id = 'redshift',
-    sql_query = SqlQueries.song_table_insert
+    sql_query = SqlQueries.song_table_insert,
+    table_name = "songs"
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
     dag=dag,
     redshift_conn_id = 'redshift',
-    sql_query = SqlQueries.artist_table_insert
+    sql_query = SqlQueries.artist_table_insert,
+    table_name = "artists"
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
     dag=dag,
     redshift_conn_id = 'redshift',
-    sql_query = SqlQueries.time_table_insert
+    sql_query = SqlQueries.time_table_insert,
+    table_name = "time"
 )
 
 run_quality_checks = DataQualityOperator(
