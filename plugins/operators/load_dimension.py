bb5114bdf006cae0ad564b8963e8c9d625a63822
karenlo08 <49169212+karenlo08@users.noreply.github.com>
2020-07-14 10:51:12 -0700

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -8,15 +8,19 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 sql_query = "",
+                 table_name = "",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_query = sql_query
+        self.table_name = table_name
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift_hook = PostgresHook(postgres_conn_id = self.redshift_conn_id) 
+        self.log.info(f"Running query to load data into Dimension Table {self.table_name}")
+        redshift_hook.run(self.sql_query)
+        self.log.info(f"Dimension Table {self.table_name} loaded.")
+
